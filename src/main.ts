0aa56469ec5a6f55610c6dd5d3b1521f68e8d8f1
RoberthFurtadoDev <roberthfurtadodev@outlook.com>
2025-06-30 01:40:53 -0300

    img

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// Importar Chart e registerables aqui para registro global
 import { Chart, registerables } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-// Registrar todos os componentes padrão do Chart.js e o plugin datalabels globalmente
 Chart.register(...registerables);
 Chart.register(ChartDataLabels);
 
